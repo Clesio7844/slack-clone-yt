a1841f182e5adef207be74d68ffe4380f2aadb47
david <daviddecastro42@live.co.uk>
2021-04-20 18:52:05 +0100

    first commit and clean the app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBIC5KLDrqLCFqf1C6SFmJQED7mPrRzozQ',
+  authDomain: 'slack-clone-yt-c2218.firebaseapp.com',
+  projectId: 'slack-clone-yt-c2218',
+  storageBucket: 'slack-clone-yt-c2218.appspot.com',
+  messagingSenderId: '570955479255',
+  appId: '1:570955479255:web:e42cb2655fcea178a49892'
+};
