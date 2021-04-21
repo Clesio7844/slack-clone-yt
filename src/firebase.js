cc0697ceef3647bc7c78ec386c894f1d4a172c93
david <daviddecastro42@live.co.uk>
2021-04-21 19:59:17 +0100

    finished the sidebar and user reduce to store the the channel and select the channel

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBIC5KLDrqLCFqf1C6SFmJQED7mPrRzozQ',
   authDomain: 'slack-clone-yt-c2218.firebaseapp.com',
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: '570955479255',
   appId: '1:570955479255:web:e42cb2655fcea178a49892'
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider, db };
